916113fb57c5e03e227189291fb7cf2c00a7afc9
Sotaro YAMAGUCHI <57104640+sotaro-ac@users.noreply.github.com>
2025-03-16 23:16:49 +0900

    style: CSharper (formatter) で整形

--- Assets/Scripts/TextDamageController.cs
+++ Assets/Scripts/TextDamageController.cs
@@ -1,17 +1,10 @@
 using UnityEngine;
 
-
 public class TextDamageController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
+    void Start() { }
 
     // Update is called once per frame
-    void Update()
-    {
-
-    }
+    void Update() { }
 }
