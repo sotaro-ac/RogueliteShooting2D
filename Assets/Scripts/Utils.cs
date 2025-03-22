984a032490ca6a55dba8a824c225019ac4db40df
Sotaro YAMAGUCHI <57104640+sotaro-ac@users.noreply.github.com>
2025-03-22 23:34:30 +0900

    feat(Util): 当たり判定のあるタイルかどうか調べる

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 // ゲームで使う共通処理をまとめたクラス
 public static class Utils
@@ -12,5 +13,19 @@ public static class Utils
         int seconds = (int)timer % 60;
         int minutes = (int)timer / 60;
         return minutes.ToString() + ":" + seconds.ToString("00");
+    }
+
+    // 当たり判定のあるタイルかどうか調べる
+    public static bool IsColliderTile(Tilemap tilemapCollider, Vector2 position)
+    {
+        // セル位置に変換
+        Vector3Int cellPosition = tilemapCollider.WorldToCell(position);
+
+        // 当たり判定あり
+        if (tilemapCollider.GetTile(cellPosition))
+        {
+            return true;
+        }
+        return false;
     }
 }
