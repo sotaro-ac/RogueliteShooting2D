8a18ef73b70bb0d2b11eb158e66fb246cec6a67d
Sotaro YAMAGUCHI <57104640+sotaro-ac@users.noreply.github.com>
2025-03-22 20:31:30 +0900

    feat(UI): ダメージテキストを敵上に表示させる(wip)

--- Assets/Scripts/TextDamageController.cs
+++ Assets/Scripts/TextDamageController.cs
@@ -1,10 +1,52 @@
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 public class TextDamageController : MonoBehaviour
 {
+    float destroyTime = 1;
+    GameObject target;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() { }
+    void Start()
+    {
+        // 膨らんで小さくなって消える
+        transform
+            .DOScale(new Vector2(1, 1), destroyTime / 2)
+            .SetRelative()
+            .OnComplete(() =>
+            {
+                transform
+                    .DOScale(new Vector2(0, 0), destroyTime / 2)
+                    .OnComplete(() => Destroy(gameObject));
+            });
+    }
 
     // Update is called once per frame
-    void Update() { }
+    void Update()
+    {
+        if (!target)
+        {
+            return;
+        }
+
+        // テキスト位置を更新
+        Vector3 pos = RectTransformUtility.WorldToScreenPoint(
+            Camera.main,
+            target.transform.position
+        );
+        transform.position = pos;
+    }
+
+    public void Init(GameObject target, float damage)
+    {
+        this.target = target;
+        TextMeshProUGUI text = GetComponent<TextMeshProUGUI>();
+
+        text.text = "" + (int)damage;
+        if (target.GetComponent<PlayerController>())
+        {
+            text.color = Color.red;
+        }
+    }
 }
