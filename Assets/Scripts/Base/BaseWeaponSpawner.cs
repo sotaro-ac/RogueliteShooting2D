2a6c0306a825560468094d0f9c880eccbd529f74
Sotaro YAMAGUCHI <57104640+sotaro-ac@users.noreply.github.com>
2025-03-27 22:51:53 +0900

    feat(Weapon): 武器生成装置の生成処理を追加する

--- Assets/Scripts/Base/BaseWeaponSpawner.cs
+++ Assets/Scripts/Base/BaseWeaponSpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseWeaponSpawner : MonoBehaviour
@@ -15,9 +16,67 @@ public class BaseWeaponSpawner : MonoBehaviour
     // 稼働タイマー
     public float TotalTimer;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() { }
+    // 生成した武器のリスト
+    public List<BaseWeapon> weapons;
 
-    // Update is called once per frame
-    void Update() { }
+    // 敵生成装置
+    protected EnemySpawnerController enemySpawner;
+
+    // 初期化
+    public void Init(EnemySpawnerController enemySpawner, WeaponSpawnerStats stats)
+    {
+        // 変数初期化
+        weapons = new List<BaseWeapon>();
+        this.enemySpawner = enemySpawner;
+        this.Stats = stats;
+    }
+
+    // 稼働タイマー
+    private void FixedUpdate()
+    {
+        TotalTimer += Time.fixedDeltaTime;
+    }
+
+    // 武器生成
+    protected BaseWeapon CreateWeapon(Vector3 position, Vector2 forward, Transform parent = null)
+    {
+        // 生成
+        GameObject obj = Instantiate(
+            PrefabWeapon,
+            position,
+            PrefabWeapon.transform.rotation,
+            parent
+        );
+        // 共通データセット
+        BaseWeapon weapon = obj.GetComponent<BaseWeapon>();
+        // データ初期化
+        weapon.Init(this, forward);
+        // 武器リストへ追加
+        weapons.Add(weapon);
+
+        return weapon;
+    }
+
+    // 武器生成（簡略化版）
+    protected BaseWeapon CreateWeapon(Vector3 position, Transform parent = null)
+    {
+        return CreateWeapon(position, Vector2.zero, parent);
+    }
+
+    // 武器のUpdateを停止する
+    public void SetEnabled(bool enabled = true)
+    {
+        this.enabled = enabled;
+        // オブジェクトを削除
+        weapons.RemoveAll(item => !item);
+        // 生成した武器を停止
+        foreach (var item in weapons)
+        {
+            item.enabled = enabled;
+            // Rigidbodyを停止
+            item.GetComponent<Rigidbody2D>().simulated = enabled;
+        }
+    }
+
+    // TODO: レベルアップ時のデータを返す
 }
--- Assets/Scripts/Settings/WeaponSpawnerSettings.cs
+++ Assets/Scripts/Settings/WeaponSpawnerSettings.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 // [CreateAssetMenu(
@@ -7,6 +8,82 @@ using UnityEngine;
 // )]
 // public class WeaponSpawnerSettings : ScriptableObject { }
 
+// 右クリックメニューに表示
+// filename はデフォルトのファイル名
+[CreateAssetMenu(
+    fileName = "WeaponSpawnerSettings",
+    menuName = "Scriptable Objects/WeaponSpawnerSettings"
+)]
+public class WeaponSpawnerSettings : ScriptableObject
+{
+    // データ
+    public List<WeaponSpawnerStats> datas;
+    static WeaponSpawnerSettings instance;
+    public static WeaponSpawnerSettings Instance
+    {
+        get
+        {
+            if (!instance)
+            {
+                instance = Resources.Load<WeaponSpawnerSettings>(nameof(WeaponSpawnerSettings));
+            }
+
+            return instance;
+        }
+    }
+
+    // リストのIDからデータを検索する
+    public WeaponSpawnerStats Get(int id, int lv)
+    {
+        // 指定されたレベルのデータがなければ最も高いレベルのデータを返す
+        WeaponSpawnerStats ret = null;
+
+        foreach (var item in datas)
+        {
+            if (id != item.Id)
+            {
+                continue;
+            }
+
+            // 指定レベルと一致
+            if (lv == item.Lv)
+            {
+                return (WeaponSpawnerStats)item.GetCopy();
+            }
+
+            // 仮のデータがセットされていないか調べ、超えるレベルであれば入れ替える
+            if (null == ret)
+            {
+                ret = item;
+            }
+            // 探しているレベル未満であり、暫定データより大きい
+            else if (item.Lv < lv && ret.Lv < item.Lv)
+            {
+                ret = item;
+            }
+        }
+
+        return (WeaponSpawnerStats)datas.Find(item => item.Id == id).GetCopy();
+    }
+
+    // 作成
+    public BaseWeaponSpawner CreateWeaponSpawner(
+        int id,
+        EnemySpawnerController enemySpawner,
+        Transform parent = null
+    )
+    {
+        // データ取得
+        WeaponSpawnerStats stats = Instance.Get(id, 1);
+        // オブジェクト作成
+        GameObject obj = Instantiate(stats.PrefabSpawner, parent);
+        // データをセット
+        BaseWeaponSpawner spawner = obj.GetComponent<BaseWeaponSpawner>();
+        spawner.Init(enemySpawner, stats);
+
+        return spawner;
+    }
+}
 
 // 武器生成装置
 [Serializable]
